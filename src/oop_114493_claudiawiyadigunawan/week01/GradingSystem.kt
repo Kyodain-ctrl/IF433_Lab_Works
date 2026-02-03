c3b968a3c9ed9f6bd848397634cbe520bbcff588
Yansev <claudiawiyadi.g@gmail.com>
2026-02-03 14:29:28 +0700

    week01: initial setup and procedural code

---
+++ src/oop_114493_claudiawiyadigunawan/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_114493_claudiawiyadigunawan.week01
+
+fun main () {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println ("Nama: " + name + ", Nilai: " + score)
+}
