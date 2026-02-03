9da102c3154a4cac9f5ba7382e8207e51e10fd8c
Yansev <claudiawiyadi.g@gmail.com>
2026-02-03 20:24:28 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_114493_claudiawiyadigunawan/week01/GameStore.kt
+++ src/oop_114493_claudiawiyadigunawan/week01/GameStore.kt
@@ -3,4 +3,10 @@ package oop_114493_claudiawiyadigunawan.week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 650000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price - (price * 20/100)
+    else price - (price * 10/100)
