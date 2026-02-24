9a2e563b050efbe5838ebf15eb9162bacb9c4f6a
Yansev <claudiawiyadi.g@gmail.com>
2026-02-24 15:20:09 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_114493_claudiawiyadigunawan/week04/Developer.kt
@@ -0,0 +1,7 @@
+package oop_114493_claudiawiyadigunawan.week04
+
+class Developer(name: String, baseSalary: Int, val programmingLanguage: String): Employee(name, baseSalary) {
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage")
+    }
+}
