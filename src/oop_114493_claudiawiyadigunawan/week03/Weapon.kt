bb9c62a9af47198a4a61c90365dc70dddea440cf
Yansev <claudiawiyadi.g@gmail.com>
2026-02-24 13:22:48 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_114493_claudiawiyadigunawan/week03/Weapon.kt
@@ -0,0 +1,24 @@
+package oop_114493_claudiawiyadigunawan.week03
+
+class Weapon(val name: String) {
+    var damage: Int = 0
+        set(value) {
+            when {
+                value < 0 -> {
+                    println("Damage tidak boleh negatif!")
+                }
+                value > 1000 -> {
+                    field = 1000
+                }
+                else -> {
+                    field = value
+                }
+            }
+        }
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
