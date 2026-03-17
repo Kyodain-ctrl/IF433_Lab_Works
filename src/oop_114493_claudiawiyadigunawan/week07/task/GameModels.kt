8318c621b6b961d9662ad60adea78c0290a83965
Yansev <claudiawiyadi.g@gmail.com>
2026-03-17 13:00:41 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_114493_claudiawiyadigunawan/week07/task/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_114493_claudiawiyadigunawan.week07.task
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
