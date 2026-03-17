8fcfe1c2d01ad3b9d3b48a3da60f0a0c518f25b3
Yansev <claudiawiyadi.g@gmail.com>
2026-03-17 12:47:16 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_114493_claudiawiyadigunawan/week07/Main.kt
+++ src/oop_114493_claudiawiyadigunawan/week07/Main.kt
@@ -23,6 +23,14 @@ fun main() {
     val (userName, userAge) = data1
     println("Destructed: $userName berumur $userAge")
 
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+
+    val uiMessage = when(response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+    }
+
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient("https://api.umn.ac.id")
     client.connect()
