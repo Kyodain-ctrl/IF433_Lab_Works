c1090d954ee91cf9352113beb9c7b9c11ff52136
Yansev <claudiawiyadi.g@gmail.com>
2026-03-17 12:58:54 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_114493_claudiawiyadigunawan/week07/task/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_114493_claudiawiyadigunawan.week07.task
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
