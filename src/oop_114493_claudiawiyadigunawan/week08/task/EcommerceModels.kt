a44f3efd4379734116b66e5f9c3d871661869e61
Yansev <claudiawiyadi.g@gmail.com>
2026-04-20 17:32:28 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_114493_claudiawiyadigunawan/week08/task/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_114493_claudiawiyadigunawan.week08.task
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
