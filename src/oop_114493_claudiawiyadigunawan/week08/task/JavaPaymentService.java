7a446d640b1fd36fd2ae0993517dd6a4f1bebad3
Yansev <claudiawiyadi.g@gmail.com>
2026-04-20 17:37:25 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_114493_claudiawiyadigunawan/week08/task/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_114493_claudiawiyadigunawan.week08.task;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
