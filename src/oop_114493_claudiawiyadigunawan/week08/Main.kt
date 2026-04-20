682ee89bab47dbeb3aa87434834132d350e65260
Yansev <claudiawiyadi.g@gmail.com>
2026-04-20 16:26:12 +0700

    week08: create mixed data collection

--- src/oop_114493_claudiawiyadigunawan/week08/Main.kt
+++ src/oop_114493_claudiawiyadigunawan/week08/Main.kt
@@ -7,7 +7,7 @@ fun main() {
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
 
-    println("/n=== TEST LET BLOCK ===")
+    println("\n=== TEST LET BLOCK ===")
     val validOrder = Order(null, 250000)
 
     val receipt = validOrder.totalPrice?.let { price ->
@@ -16,4 +16,13 @@ fun main() {
     } ?: "Transaksi Invalid: Harga belum di-set!"
 
     println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }
