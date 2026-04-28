4a8ad517c7f7c3acfe840f5492b5b14b986090f1
Yansev <claudiawiyadi.g@gmail.com>
2026-04-28 14:13:47 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_114493_claudiawiyadigunawan/week10/tugas/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_114493_claudiawiyadigunawan.week10.tugas
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
