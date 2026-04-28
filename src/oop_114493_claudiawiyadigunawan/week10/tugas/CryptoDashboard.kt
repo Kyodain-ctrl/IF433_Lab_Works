6ec4346f59c2fd1ab33dbf2593a48da8bc849e6d
Yansev <claudiawiyadi.g@gmail.com>
2026-04-28 14:22:30 +0700

    week10: (task) populate coin repository

--- src/oop_114493_claudiawiyadigunawan/week10/tugas/CryptoDashboard.kt
+++ src/oop_114493_claudiawiyadigunawan/week10/tugas/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_114493_claudiawiyadigunawan.week10.tugas
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
