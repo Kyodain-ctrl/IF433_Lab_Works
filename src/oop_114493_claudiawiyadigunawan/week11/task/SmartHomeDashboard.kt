ea87d7b1349d738f99d7fbe3f5df7e0afde82239
Yansev <claudiawiyadi.g@gmail.com>
2026-05-05 14:56:38 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_114493_claudiawiyadigunawan/week11/task/SmartHomeDashboard.kt
+++ src/oop_114493_claudiawiyadigunawan/week11/task/SmartHomeDashboard.kt
@@ -10,4 +10,14 @@ fun main() {
     }.also {
         homeDevices.add(it)
     }
+
+    val camera = SmartDevice("Ezviz Outdoor", "Camera")
+        .apply {
+            isOnline = true
+            powerLoad = 5
+        }
+        .also {
+            println("(LOG) Kamera terhubung")
+            homeDevices.add(it)
+        }
 }
