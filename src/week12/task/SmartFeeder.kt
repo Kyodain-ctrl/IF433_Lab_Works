9190d919e5878ce21d5d1bdf3734fee7569883bb
Yansev <claudiawiyadi.g@gmail.com>
2026-05-12 14:41:38 +0700

    week12: (task) implement hardware validation with throw

--- src/week12/task/SmartFeeder.kt
+++ src/week12/task/SmartFeeder.kt
@@ -10,5 +10,10 @@ fun dispenseKibble(
         "Porsi kibble harus lebih dari 0 gr"
     }
 
+    if (isJammed) {
+
+        throw DispenserJamException()
+    }
+
     return availableGram - requestedGram
 }
