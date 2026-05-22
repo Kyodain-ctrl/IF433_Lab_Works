ba7c79c57a7a39c30097dc860de9c2b98352ec2a
Yansev <claudiawiyadi.g@gmail.com>
2026-05-22 09:41:08 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_114493_claudiawiyadigunawan/week13/task/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_114493_claudiawiyadigunawan.week13.task
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
