65dd6de396812b40a5a7a73e9f5ddba335551791
Yansev <claudiawiyadi.g@gmail.com>
2026-05-22 09:51:00 +0700

    week13: (task) inject malformed line via appendText to test robustness

--- src/oop_114493_claudiawiyadigunawan/week13/task/CryptoTradeLogger.kt
+++ src/oop_114493_claudiawiyadigunawan/week13/task/CryptoTradeLogger.kt
@@ -56,4 +56,6 @@ fun main() {
         TradeRecord(3, "SOLUSDT", "Long", 500.0, 75.0)
     )
     saveTrades(trades, "crypto_trades.csv")
+
+    File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
 }
