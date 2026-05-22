9234b131e5759eae80a0b995cc38758524366ec3
Yansev <claudiawiyadi.g@gmail.com>
2026-05-22 09:52:56 +0700

    week13: (task)test full crypto trade logger pipeline

--- src/oop_114493_claudiawiyadigunawan/week13/task/CryptoTradeLogger.kt
+++ src/oop_114493_claudiawiyadigunawan/week13/task/CryptoTradeLogger.kt
@@ -64,4 +64,10 @@ fun main() {
     val totalPnl = loadedData.sumOf {
         it.pnl
     }
+
+    loadedData.forEach {
+        println(it)
+    }
+
+    println("=== TOTAL PnL BERSIH: $totalPnl ===")
 }
