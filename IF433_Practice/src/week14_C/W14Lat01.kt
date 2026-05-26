f7d1abfd35380d7f101a1cb7915bf9fa2d4c203c
Yansev <claudiawiyadi.g@gmail.com>
2026-05-26 13:30:01 +0700

    week14: create DiscountStrategy interface and implementations

--- IF433_Practice/src/week14_C/W14Lat01.kt
+++ IF433_Practice/src/week14_C/W14Lat01.kt
@@ -7,6 +7,9 @@ class proses_khs {
     fun hitung_nilai_akhir(uts: Double, uas: Double, tugas: Double):Double {
         return (uts * 0.3) + (uas * 0.4) + (tugas * 0.3)
     }
+}
+
+class db_khs {
     fun simpan_na_db(nim: String, nama: String, nilaiAkhir: Double): String {
         return "KHS $nim nama $nama dapat nilai $nilaiAkhir"
     }
@@ -20,6 +23,7 @@ fun main() {
     val nilaiTugas:Double = khsMhs.hitung_nilai_tugas(83.57, 76.45)
     val naMhs:Double = khsMhs.hitung_nilai_akhir(85.67, 70.40, nilaiTugas)
 
-    val statSimpan:String = khsMhs.simpan_na_db("123", "Claudia", naMhs)
+    val dbMhs = db_khs()
+    val statSimpan:String = dbMhs.simpan_na_db("123", "Claudia", naMhs)
     println(statSimpan)
 }
